67dbae906001e138a5a1abccb1bed0503d68ba4b
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-04-03 18:46:29 -0300

    Foi criado Menu(Navbar) seguindo o modelo elaborado
    nao esta pronto.
    
    tambem está sendo commitando junto um modelo de organizacao de pastas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import './App.css';
+
+
+function App(){
+    
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
