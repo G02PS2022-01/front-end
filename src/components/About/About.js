301fadbbe7524860cd37c9439227eec43feda477
Wanderson Bueno <wanolibueno@gmail.com>
2022-04-04 10:13:51 -0300

    Change the About us page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <>
       <div className="container">
         <div className="imgAbout">
-          <img src="#" alt="" />
+          <img src="https://wandersonbueno.com.br/ps/dogCode.svg" alt="" />
         </div>
         <div className="textAbout">
           <h2>Um pouco sobre nossa equipe e projeto.</h2>
@@ -27,53 +27,6 @@ const About = () => {
             </li>
           </ul>
         </div>
-        <section className="avatar">
-          <div className="avatarSingle">
-            <img
-              src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
-              alt=""
-            />
-            <p>
-              <a href="#">@amaur1mmj</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./././avatar.png" alt="" />
-            <p>
-              <a href="#">@vinnieoh</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./avatar.png" alt="" />
-            <p>
-              <a href="#">@gabrielgcarvalho</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./avatar.png" alt="" />
-            <p>
-              <a href="#">@wandbueno</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./avatar.png" alt="" />
-            <p>
-              <a href="#">@Marcos1079</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./avatar.png" alt="" />
-            <p>
-              <a href="#">@nicolasarthur10</a>
-            </p>
-          </div>
-          <div className="avatarSingle">
-            <img src="./avatar.png" alt="" />
-            <p>
-              <a href="#">@gustavo</a>
-            </p>
-          </div>
-        </section>
       </div>
     </>
   )
