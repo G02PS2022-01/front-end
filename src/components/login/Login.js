5a1937effe9ff36cde4409fa91a2b0fee838eb0d
marcos1079 <marcos.antonio2306@outlook.com>
2022-04-16 16:19:26 -0300

    correcções no front-end da tela de login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './login.css';
+import imagemEmail from "../../assets/img/email.png";
+import imagemGoogle from "../../assets/img/google.png";
 
 const Login = ()=>{
     return(
@@ -10,7 +12,7 @@ const Login = ()=>{
             <div className="buttonEntrar">
                 <a href="#">
                         Entrar com Google
-                <img src="https://s3-alpha-sig.figma.com/img/79c6/a04d/7dff82097a22ce080aa49aa3352fd7a2?Expires=1649635200&Signature=H7Mgnuv~XeeIi9t4PicBHPimcdRbHLym7xFUBGbm8-VlN4reDkuXQzP4RjD9chr8K~NQIB6e40sPRjKxxVM6UxmV3rtkjY2u7~mDreJvSdOSMH40H246Xg8nOI5dLcDFdAimL4PtryT45jkhZoZ45iolU2W51UC4fXmxfrrEENXHs4I-K7AxjpN29UYhF3LsEQdHz3gy7O4sER5emjkXjFdO0wOoh3lNc0YUBme~Uwy77CM3Xc4ByPozPeW8kRBFDbIOVeE3g6HS2zo6V2XqwdevO7N-aQdRAAd5WTjMWH0E31vAqjzzEtzhcB50o6jpkHEj4Ppmojc4GWgN~9E2ww__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"/> 
+                <img src={imagemGoogle}/> 
                 </a>
             </div>
 
@@ -19,7 +21,7 @@ const Login = ()=>{
 
                         Sign e-mail
                     
-                <img src="https://s3-alpha-sig.figma.com/img/f5b7/7a67/dad620340392059cf882b91b517bc41e?Expires=1649635200&Signature=Xbg4GQ38IwmdEl2lHon4wGYG02ftdVetvqAqOMpqm0BlG3fKWTZinkGlCIpn9AlLhDgcXObsLfH~E8s9~HMy0oONgvquYpAuYBRqfZ47sHmsNP-G0p9keRs5l0BDjVBNGsmhw1StqWE4Xn4QOAP0uDx4xWh3NPgmb8Wx-WnSP0kLcCXVHhS5Lsf~rhO5Nqz5FChDFNZ8AnVR94ZBL61C4UrWUu-YB5DRQ6~pp1jkzyXLucCwp367uCkyt~vXNTdayPDwRQs-JxoNLS4jueTR9ELwO5dlge21FA4qG5pCylfB7oEi7X~1zjYZdHngNfJXIdj3S0F07ORX8tzj3ICmSg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"/> 
+                <img src={imagemEmail}/> 
                 </a>
             </div>
                 <div className="novoUsuario">
@@ -30,9 +32,7 @@ const Login = ()=>{
             
         
         </div>
-        <div className="imageLogin">
-                <img src="https://s3-alpha-sig.figma.com/img/bbd3/a475/85446aeb328f11d1f66a9ae7c1b29535?Expires=1649635200&Signature=PRXk-uu03Aj619fvMTrBUfGGJOVsz84OPgVh82X85~IqUWuM-yDo1JEZECIVF1O58kGY0-5Hh-X0nNUwa~rKzH1btlRqgDBZhvEkAW91ErraHnFv6bMp7z~VKAd3rs6rL~9qNrvoiWKRHRsyLfGWGuUdMoUbmKwRyvzYdA5NxGzedqkkWQI7yaSDCNQUrMGWYV~9ejdBIlolIC75HYFyktZsWmpt9Z18scp0GcoL5ZEw8Kd1CF4dAjJRDFg5V2HsRi-oZeVwqgiiHK8OsIF1FQ-0NxKCYWShqfzz4vVk5oTbPq1X2bXCkHiKrvQxtuKXghuRYKMeMYHkgYBQc9pMtg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
-        </div>
+        
     </div>
     )
 }
