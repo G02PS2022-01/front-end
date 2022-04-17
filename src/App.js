982f23266e1ba3a37856c632bc1cc9176c9f911a
Wanderson Bueno <wanolibueno@gmail.com>
2022-04-17 01:48:29 -0300

    I02/R19 - Passo a passo
    Criação da sessão passo a passo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './layouts/Navbar/Navbar'
 import About from './pages/About/About'
+import Step from './pages/Home-page/Step/Step'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import IndexFooter from './layouts/Footer/IndexFooter';
-import Home from './pages/Home'
+import IndexFooter from './layouts/Footer/IndexFooter'
+import Home from './pages/Home-page/Home'
 import SignIn from './pages/SignIn'
 import WhatCoding from './pages/WhatCoding'
 import AboutUs from './pages/About/AboutUs'
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Navbar />
       <About />
+      <Step />
       <Routes>
         <Route path="/" exact component={Home} />
         <Route path="what-coding" component={WhatCoding} />
