46693442e46fc3c113ea4d354cc770599965521d
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-04-18 14:25:04 -0300

    atualizações da feature R13

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './layouts/Navbar/Navbar'
-import About from './pages/About/About'
-import Step from './pages/Home-page/Step/Step'
+//import About from './pages/About/About'
+//import Step from './pages/Home-page/Step/Step'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import IndexFooter from './layouts/Footer/IndexFooter'
 import Home from './pages/Home-page/Home'
 import SignIn from './pages/SignIn'
-import WhatCoding from './pages/WhatCoding'
+import WhatCoding from './pages/WhatCoding/WhatCoding'
 import AboutUs from './pages/About/AboutUs'
 import Curriculum from './pages/Curriculum'
 import Glossary from './pages/Glossary'
@@ -17,8 +17,10 @@ function App() {
     
     <Router>
       <Navbar />
-      <About />
-      <Step />
+
+     <AboutUs />
+     <WhatCoding />
+
       <Routes>
         <Route path="/" exact element={Home} />
         <Route path="what-coding" element={WhatCoding} />
@@ -27,6 +29,7 @@ function App() {
          <Route path="glossary" element={Glossary} />
          <Route path="sign-in" element={SignIn} />  
       </Routes>
+
       <IndexFooter />
     </Router>
 
