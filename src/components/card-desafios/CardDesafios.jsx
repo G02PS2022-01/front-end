fd6b9a3057a2927a113564815bac623d13ff4c91
vinnieoh <viniciusoliveira926@gmail.com>
2022-04-28 09:59:40 -0300

    regra de nogoio botao

--- src/components/card-desafios/CardDesafios.jsx
+++ src/components/card-desafios/CardDesafios.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import './style.css';
 import CardBloqueado from "../../assets/img/Card-Desafios-Bloqueado.png";
@@ -8,12 +8,17 @@ import CardDesloqueado from "../../assets/img/Card-Desafios-Desbloqueado.png";
 export default function CardDesafios(){
 
     const history = useHistory();
+    const [disable, setDisable] = useState(false);
 
+    const nivel = 1;
 
     return (
         <>
         <section className="section" id="passos">
             <div className="container grid">
+                <header>
+                    <h2 class="title">Desafios de Programação</h2>
+                </header>
                 <div className="cards grid">
                     <div className="card">
                         <i className=""><img src={CardDesloqueado}/></i>
@@ -24,9 +29,9 @@ export default function CardDesafios(){
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
                     </div>
-                    <div className="card">
-                        <i className=""><img src={CardBloqueado}/></i>
-                        <button id='btn' onClick={() => history.push('#')}>
+                    <div className="card" >
+                        <i className=""><img src={(nivel >= 2) ? CardDesloqueado: CardBloqueado}/></i>
+                        <button disabled={nivel < 2 ? true:false} id='btn'onClick={() => history.push('#')}>
                             <h3 className="title">Nivel #2</h3>
                         </button>
                         <p>
@@ -34,36 +39,9 @@ export default function CardDesafios(){
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado}/></i>
+                        <i className=""><img src={(nivel >= 3) ? CardDesloqueado: CardBloqueado}/></i>
                         <button id='btn' onClick={() => history.push('#')}>
                             <h3 className="title">Nivel #3</h3>
-                        </button>
-                        <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        </p>
-                    </div>
-                    <div className="card">
-                        <i className=""><img src={CardBloqueado}/></i>
-                        <button id='btn' onClick={() => history.push('#')}>
-                            <h3 className="title">Nivel #4</h3>
-                        </button>
-                        <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        </p>
-                    </div>
-                    <div className="card">
-                        <i className=""><img src={CardBloqueado}/></i>
-                        <button id='btn' onClick={() => history.push('#')}>
-                            <h3 className="title">Nivel #5</h3>
-                        </button>
-                        <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        </p>
-                    </div>
-                    <div className="card">
-                        <i className=""><img src={CardBloqueado}/></i>
-                        <button id='btn' onClick={() => history.push('#')}>
-                            <h3 className="title">Nivel #6</h3>
                         </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
