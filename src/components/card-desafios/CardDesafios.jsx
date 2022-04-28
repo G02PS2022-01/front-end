41b6b5e39b138a2fce1c41b0ca460dd0880ae27d
vinnieoh <viniciusoliveira926@gmail.com>
2022-04-27 21:08:12 -0300

    atualizacao botoes

--- src/components/card-desafios/CardDesafios.jsx
+++ src/components/card-desafios/CardDesafios.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './style.css';
 import CardBloqueado from "../../assets/img/Card-Desafios-Bloqueado.png";
@@ -16,8 +16,8 @@ export default function CardDesafios(){
             <div className="container grid">
                 <div className="cards grid">
                     <div className="card">
-                        <i className=""><img src={CardDesloqueado} /></i>
-                        <button className='btn' onClick={() => history.push('#')}>
+                        <i className=""><img src={CardDesloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
                             <h3 className="title">Nivel #1</h3>
                         </button>
                         <p>
@@ -25,36 +25,46 @@ export default function CardDesafios(){
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado} /></i>
-                        <h3 className="title">Nivel #2</h3>
+                        <i className=""><img src={CardBloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
+                            <h3 className="title">Nivel #2</h3>
+                        </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado}  /></i>
-                        <h3 className="title">Nivel #3</h3>
+                        <i className=""><img src={CardBloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
+                            <h3 className="title">Nivel #3</h3>
+                        </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado}  /></i>
-                        <h3 className="title">Nivel #4</h3>
+                        <i className=""><img src={CardBloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
+                            <h3 className="title">Nivel #4</h3>
+                        </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado}  /></i>
-                        <h3 className="title">Nivel #5</h3>
+                        <i className=""><img src={CardBloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
+                            <h3 className="title">Nivel #5</h3>
+                        </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
                     </div>
                     <div className="card">
-                        <i className=""><img src={CardBloqueado}  /></i>
-                        <h3 className="title">Nivel #6</h3>
+                        <i className=""><img src={CardBloqueado}/></i>
+                        <button id='btn' onClick={() => history.push('#')}>
+                            <h3 className="title">Nivel #6</h3>
+                        </button>
                         <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         </p>
