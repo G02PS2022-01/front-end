11b66a5bfdf07c8c6d9bcbbf4b385a14eba5c773
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-05-01 14:01:39 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import IndexFooter from './layouts/Footer/IndexFooter'
 import Home from './pages/Home-page/Home'
 import SignIn from './pages/SignIn';
-import WhatCoding from './pages/WhatCoding'
+import WhatCoding from './pages/WhatCoding/WhatCoding'
 import AboutUs from './pages/About/AboutUs'
 import Curriculum from './pages/Curriculum'
 import Glossary from './pages/Glossary'
