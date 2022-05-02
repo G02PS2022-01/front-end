17e92152833a8857dfb1e2acf7676dc8b6c3a676
unknown <gustavo.casaes@mail.uft.edu.br>
2022-05-02 15:02:52 -0300

    correcao feature/I02/R15 na develop

--- src/pages/Home-page/Step/Step.jsx
+++ src/pages/Home-page/Step/Step.jsx
@@ -7,33 +7,33 @@ import Img03 from '../../../assets/img/sync.svg';
 export default function Step() {
   return (
     <>
-    <div class="divider-2"></div>
-      <section class="section" id="passos">
-        <div class="container grid">
+    <div className="divider-2"></div>
+      <section className="section" id="passos">
+        <div className="container grid">
           <header>
-            <h2 class="title">Passo a passo</h2>
+            <h2 className="title">Passo a passo</h2>
             <p>A melhor maneira de começar sua jornada na programação.</p>
           </header>
-          <div class="cards grid">
-            <div class="card">
-              <i class=""><img src={Img01} /></i>
-              <h3 class="title">Passo #1</h3>
+          <div className="cards grid">
+            <div className="card">
+              <i className=""><img src={Img01} /></i>
+              <h3 className="title">Passo #1</h3>
               <p>
                 Aprenda com lições rápidas e divertidas que ensinam você a
                 escrever seu primeiro código.
               </p>
             </div>
-            <div class="card">
-              <i class=""><img src={Img02} /></i>
-              <h3 class="title">Passo #2</h3>
+            <div className="card">
+              <i className=""><img src={Img02} /></i>
+              <h3 className="title">Passo #2</h3>
               <p>
                 Percorra níveis progressivamente desafiadores à medida que
                 desenvolve suas habilidades.
               </p>
             </div>
-            <div class="card">
-              <i class=""><img src={Img03} /></i>
-              <h3 class="title">Passo #3</h3>
+            <div className="card">
+              <i className=""><img src={Img03} /></i>
+              <h3 className="title">Passo #3</h3>
               <p>
                 Crie habilidades de programação fundamentais para o seu próximo
                 passo como programador.
@@ -42,7 +42,7 @@ export default function Step() {
           </div>
         </div>
       </section>
-      <div class="divider-1"></div>
+      <div className="divider-1"></div>
   
     </>
   );
