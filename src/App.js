1797a54af76c4fc9f3c02dec164755499f682938
Wanderson Bueno <wanolibueno@gmail.com>
2022-05-07 00:23:59 -0300

    finalização da funcionalidade da pagina de dicionario

--- src/App.js
+++ src/App.js
@@ -4,28 +4,26 @@ import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import IndexFooter from './layouts/Footer/IndexFooter'
 import Home from './pages/Home-page/Home'
-import SignIn from './pages/SignIn';
+import SignIn from './pages/SignIn'
 import WhatCoding from './pages/WhatCoding'
 import AboutUs from './pages/About/AboutUs'
 import Curriculum from './pages/Curriculum'
-import Glossary from './pages/Glossary'
+import Glossary from './pages/Glossary/Glossary'
 
 function App() {
   return (
-    
     <Router>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/what-coding" component={WhatCoding} />
-         <Route exact path="/about-us" component={AboutUs} /> 
-         <Route exact path="/curriculum" component={Curriculum} />
-         <Route exact path="/glossary" component={Glossary} />
-         <Route exact path="/sign-in" component={SignIn} />  
+        <Route exact path="/about-us" component={AboutUs} />
+        <Route exact path="/curriculum" component={Curriculum} />
+        <Route exact path="/glossary" component={Glossary} />
+        <Route exact path="/sign-in" component={SignIn} />
       </Switch>
       <IndexFooter />
     </Router>
-
   )
 }
 
---
+++ src/pages/Glossary/Glossary.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import Conteudo from './conteudo/Conteudo'
+import './Glossary.css'
+
+export default function Glossary() {
+  const [item, setItem] = useState('data')
+
+  const SideMenu = () => {
+    const SUMMARY = [
+      {
+        index: 0,
+        id: 'data',
+        name: 'Tipos de Dados',
+        title: 'Tipos de Dados'
+      },
+      {
+        index: 1,
+        id: 'functions',
+        name: 'functions',
+        title: 'Funções'
+      },
+      { index: 2, id: 'variables', name: 'variables', title: 'Variáveis' },
+      { index: 3, id: 'math', name: 'math', title: 'Operadores Aritméticos' },
+      {
+        index: 4,
+        id: 'assignment',
+        name: 'assignment',
+        title: 'Operadores de Atribuição'
+      },
+      {
+        index: 5,
+        id: 'comparison',
+        name: 'comparison',
+        title: 'Operadores de comparação'
+      },
+      {
+        index: 7,
+        id: 'logical',
+        name: 'logical',
+        title: 'Operadores lógicos'
+      }
+    ]
+    return (
+      <div className="list ">
+        <ul id="sidemenu">
+          <div>
+            <h2 className="title">Dicionário</h2>
+          </div>
+          {SUMMARY.map((item, index) => {
+            return (
+              <li
+                key={item.index}
+                id={item.id}
+                onClick={() => handleSelect(item.name)}
+              >
+                <a className="subtitle" href="#">
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+  const handleSelect = item => {
+    // console.log(item);
+    setItem(item)
+  }
+
+  return (
+    <>
+      <section className="section container " id="glossary">
+        <div className="containerGlossary grid">
+          {/* Sidebar Menu */}
+          {SideMenu()}
+          {/* Infocomponets */}
+          <Conteudo itemSelected={item} />
+        </div>
+      </section>
+    </>
+  )
+}
