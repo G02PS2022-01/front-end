0d3b28587c8f5572d3a600a119008a4f5c77d70a
Wanderson Bueno <wanolibueno@gmail.com>
2022-05-30 00:31:08 -0300

    Criação do PopUp de Minhas configurações

--- src/components/Button-Logado/ButtonLogado.jsx
+++ src/components/Button-Logado/ButtonLogado.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import './style.css';
-
 import DogIcon from "../../assets/img/dog-icon-navbar.png";
-
+import ModalConfig from '../../components/Modals/ModalConfig'
 
 export function ButtonLogado() {
 
   const [isActive, setIsActive] = useState(false)
+  const [modalConfigOpen, setModalConfigOpen] = useState(false)
 
   return (
     <>
@@ -21,11 +21,16 @@ export function ButtonLogado() {
           <div className="dropDow-content">
             <div className="dropDow-item">Comunicar bug</div>
             <div
-              className="dropDow-item"
-             
+              className="dropDow-item openModalBtn"
+              onClick={() => {
+                setModalConfigOpen(true)
+              }}
             >
               Configurações
             </div>
+            {modalConfigOpen && (
+              <ModalConfig setOpenModalConfig={setModalConfigOpen} />
+            )}
             
             <div className="dropDow-item">Sair</div>
           </div>
